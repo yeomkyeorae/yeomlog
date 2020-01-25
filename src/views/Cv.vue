6282a7a33af8e65ba22fdb50a6c2038d2c09e5af
yeomkyeorae <duarufp06@naver.com>
2020-01-25 16:56:25 +0900

    200125 | add v-card component for CV

--- src/views/Cv.vue
+++ src/views/Cv.vue
@@ -1,11 +1,51 @@
 <template>
-  <div>
-    <h1>CV!</h1>
-  </div>
+ <v-card>
+    <v-toolbar flat color="blue-grey" dark>
+			<v-toolbar-title>Cover Letter</v-toolbar-title>
+    </v-toolbar>
+
+    <v-card-text>
+			<v-divider class="my-2"></v-divider>
+      <v-text-field filled label="Name" value="asdfasdf"></v-text-field>
+
+			<v-divider class="my-2"></v-divider>
+      <v-textarea
+        filled
+        label="Education"
+        value="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
+      >
+			</v-textarea>
+
+      <v-divider class="my-2"></v-divider>
+			<v-textarea
+        filled
+        label="Interests"
+        value="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
+      >
+			</v-textarea>
+
+			<v-divider class="my-2"></v-divider>
+			<v-textarea
+        filled
+        label="Projects list"
+        value="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt."
+      >
+			</v-textarea>
+    </v-card-text>
+
+
+
+
+  </v-card>
 </template>
 
 <script>
 export default {
-  name: 'Cv',
+	name: 'Cv',
+	data() {
+		return {
+			fullName: 'Kyeorae Yeom'
+		}
+	}
 }
 </script>
