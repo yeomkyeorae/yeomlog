67666857d560876e64a849df72e748669b3ea69f
yeomkyeorae <duarufp06@naver.com>
2020-01-29 23:22:06 +0900

    200129 | add contents of cv

--- src/views/Cv.vue
+++ src/views/Cv.vue
@@ -12,18 +12,23 @@
 		<v-divider class="my-2"></v-divider>
       
 			<h1>Education</h1>
-			<ol>
-				<li>asdf</li>
-				<li>qwer</li>
-			</ol>
+			<v-list>
+				<v-list-item>- Ajou Univ, Major: Economics & ICT convergence, 11.03 ~ 17.02</v-list-item>
+				<v-list-item>- NCIA's class for data scientist using IoT, 17.01 ~ 17.06</v-list-item>
+				<v-list-item>- Samsung Software Academy for Youth(SSAFY), 19.07 ~ ing</v-list-item>
+			</v-list>
       
 			<v-divider class="my-2"></v-divider>
 
 			<h1>skills</h1>
-			<ol>
-				<li>asdf</li>
-				<li>qwer</li>
-			</ol>
+			<v-list>
+				<v-list-item>- Python</v-list-item>
+				<v-list-item>- Javascript</v-list-item>
+				<v-list-item>- Pandas, Numpy</v-list-item>
+				<v-list-item>- InfluxDB, Grafana</v-list-item>
+				<v-list-item>- Elastic Search, Kibana</v-list-item>
+				<v-list-item>- SQL</v-list-item>
+			</v-list>
 
 			<v-divider class="my-2"></v-divider>
 
