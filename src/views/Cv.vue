e27c9d5eed5abbcaf89357836aaed1eb1c769cf7
yeomkyeorae <duarufp06@naver.com>
2020-02-02 23:05:50 +0900

    200202 | changed cv contents

--- src/views/Cv.vue
+++ src/views/Cv.vue
@@ -11,16 +11,26 @@
 
 		<v-divider class="my-2"></v-divider>
       
-			<h1>Education</h1>
+			<h1>PROFILE</h1>
 			<v-list>
 				<v-list-item>- Ajou Univ, Major: Economics & ICT convergence, 11.03 ~ 17.02</v-list-item>
 				<v-list-item>- NCIA's class for data scientist using IoT, 17.01 ~ 17.06</v-list-item>
+				<v-list-item>- Korea Electric Technology Institute(KETI), 17.07 ~ 18.06</v-list-item>
 				<v-list-item>- Samsung Software Academy for Youth(SSAFY), 19.07 ~ ing</v-list-item>
 			</v-list>
       
 			<v-divider class="my-2"></v-divider>
+      
+			<h1>CERTIFICATE</h1>
+			<v-list>
+				<v-list-item>- 정보처리기사</v-list-item>
+				<v-list-item>- 데이터분석준전문가(ADsP)</v-list-item>
+				<v-list-item>- OPIc IM2</v-list-item>
+			</v-list>
+      
+			<v-divider class="my-2"></v-divider>
 
-			<h1>skills</h1>
+			<h1>SKILLS</h1>
 			<v-list>
 				<v-list-item>- Python</v-list-item>
 				<v-list-item>- Django</v-list-item>
@@ -34,7 +44,7 @@
 
 			<v-divider class="my-2"></v-divider>
 
-			<h1>Interests</h1>
+			<h1>INTERESTS</h1>
 			<v-list>
 				<v-list-item>- Web Service(SPA)</v-list-item>
 				<v-list-item>- Data Analysis(prediction of time series)</v-list-item>
@@ -43,9 +53,9 @@
 
 			<v-divider class="my-2"></v-divider>
 
-			<h1>Projects</h1>
+			<h1>PROJECTS</h1>
 			<v-list>
-				<v-list-item>- Project 1</v-list-item>
+				<v-list-item><h2>- Project 1</h2></v-list-item>
 				<pre>Kospi200 주식 종목 클러스터링 및 예측(차세대 융합콘텐츠 산업협회)
 (2017.04 ~ 2017.06)
 설명 :코스피200 주식 종목들의 1개월 틱 단위 데이터를 확보하여
@@ -58,11 +68,10 @@
 • Python 머신러닝 라이브러리Scikit-learn 활용.
 • Hidden Markov 개념 학습.
 • 데이터분석을 위한 전체 파이프라인 구축 경험(데이터 수집 – 데이터 전처리 – 분석 – 시각화).</pre>
-				<v-list-item>- Project 2</v-list-item>
+				<v-list-item><h2>- Project 2</h2></v-list-item>
 				<pre>제지 공장 에너지 절감을 위한 데이터 분석 프로젝트(전자부품연구원)
 (2017.07 ~ 진행 중)
 설명 : 제지 공장에서 발생하는 다양한 데이터(Plant Information data, 실시간 전력 data, MES data 등)로 머신러닝 기법을 활용해 에너지 절감 포인트들을 찾는 프로젝트.   
-
 
 성과
 • 시계열 데이터 전처리(resampling, moving average) 및 핸들링.
