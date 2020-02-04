8dbe46c31e6b104d937f247b53a36d045ccd1ed1
yeomkyeorae <duarufp06@naver.com>
2020-02-04 22:58:48 +0900

    200204 | _

---
+++ src/components/Network.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+	<h1>Network</h1>
+    <MenuBar></MenuBar>
+  </div>
+</template>
+
+<script>
+import MenuBar from '@/components/MenuBar.vue'
+
+export default {
+    name: 'Network',
+    components: {
+      MenuBar
+    }
+}
+</script>
+<style>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Web.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+	<h1>Web</h1>
+    <MenuBar></MenuBar>
+  </div>
+</template>
+
+<script>
+import MenuBar from '@/components/MenuBar.vue'
+
+export default {
+    name: 'Web',
+    components: {
+      MenuBar
+    }
+}
+</script>
+<style>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
