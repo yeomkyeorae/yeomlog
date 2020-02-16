79896ff09e1671f90d28217fadda6e37d5d9abec
yeomkyeorae <duarufp06@naver.com>
2020-02-16 22:15:35 +0900

    200216 | change createfrom little bit

--- frontend/src/components/CreateForm.vue
+++ frontend/src/components/CreateForm.vue
@@ -1,101 +1,46 @@
 <template>
+  <div justify-content="center">
   <v-form
     ref="form"
-    v-model="valid"
     lazy-validation
   >
     <v-text-field
-      v-model="name"
-      :counter="10"
-      :rules="nameRules"
-      label="Name"
+      v-model="title"
+      label="title"
       required
     ></v-text-field>
 
-    <v-text-field
-      v-model="email"
-      :rules="emailRules"
-      label="E-mail"
-      required
-    ></v-text-field>
-
-    <v-select
-      v-model="select"
-      :items="items"
-      :rules="[v => !!v || 'Item is required']"
-      label="Item"
-      required
-    ></v-select>
-
-    <v-checkbox
-      v-model="checkbox"
-      :rules="[v => !!v || 'You must agree to continue!']"
-      label="Do you agree?"
-      required
-    ></v-checkbox>
+    <v-textarea
+      label="content"
+      auto-grow
+    ></v-textarea>
 
     <v-btn
-      :disabled="!valid"
       color="success"
       class="mr-4"
-      @click="validate"
     >
-      Validate
+      게시
     </v-btn>
 
     <v-btn
       color="error"
       class="mr-4"
-      @click="reset"
     >
-      Reset Form
-    </v-btn>
-
-    <v-btn
-      color="warning"
-      @click="resetValidation"
-    >
-      Reset Validation
+      취소
     </v-btn>
   </v-form>
+    </div>
 </template>
 <script>
   export default {
     name: 'CreateForm',
     data: () => ({
-      valid: true,
-      name: '',
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-      ],
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-      ],
-      select: null,
-      items: [
-        'Item 1',
-        'Item 2',
-        'Item 3',
-        'Item 4',
-      ],
-      checkbox: false,
+      title: '',
+      content: '',
     }),
 
     methods: {
-      validate () {
-        if (this.$refs.form.validate()) {
-          this.snackbar = true
-        }
-      },
-      reset () {
-        this.$refs.form.reset()
-      },
-      resetValidation () {
-        this.$refs.form.resetValidation()
-      },
+
     },
   }
 </script>
