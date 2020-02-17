b6e2eb238f2d2f8c1f2829d87c319eff0994daf0
yeomkyeorae <duarufp06@naver.com>
2020-02-17 22:25:10 +0900

    200217 | amplify CV

--- frontend/src/components/CreateForm.vue
+++ frontend/src/components/CreateForm.vue
@@ -3,12 +3,14 @@
     <v-form ref="form" lazy-validation>
       <v-text-field v-model="title" label="title" required></v-text-field>
       <v-textarea label="content" auto-grow></v-textarea>
-      <v-btn color="success" class="mr-4">게시</v-btn>
+      <v-btn color="success" class="mr-4" @click="submit">게시</v-btn>
       <v-btn color="error" class="mr-4" @click="back">취소</v-btn>
     </v-form>
   </div>
 </template>
 <script>
+// import axios from 'axios'
+
   export default {
     name: 'CreateForm',
     props: {
@@ -23,6 +25,12 @@
     methods: {
       back() {
         this.$router.push(`/interests/${this.category}`)
+      },
+      submit() {
+        // axios.post()
+        //   .then(response => {
+            
+        //   })
       }
     },
   }
