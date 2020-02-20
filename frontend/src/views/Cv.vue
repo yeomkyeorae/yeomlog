acd7aa789fb1549c058e1a89dfb2f2c984e21627
yeomkyeorae <duarufp06@naver.com>
2020-02-20 23:50:39 +0900

    200220 |

--- frontend/src/views/Cv.vue
+++ frontend/src/views/Cv.vue
@@ -43,6 +43,8 @@
 			<v-list-item>- Javascript</v-list-item>
 			<v-list-item>- Vue.js</v-list-item>
 			<v-list-item>- Pandas, Numpy</v-list-item>
+			<v-list-item>- Scikit-learn</v-list-item>
+			<v-list-item>- Tensorflow</v-list-item>
 			<v-list-item>- InfluxDB, Grafana</v-list-item>
 			<v-list-item>- Elastic Search, Kibana</v-list-item>
 			<v-list-item>- MySQL, MariaDB</v-list-item>
