d1f9acc3cdbda5ba6f4046e9139853a18b48e8a1
yeomkyeorae <duarufp06@naver.com>
2020-02-24 15:27:39 +0900

    200224 | post some pretty buy ugle

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -1,18 +1,26 @@
 <template>
-  <div>
-		{{title}}
-		{{content}}
+	<div>
+		<v-col cols="12" sm="6">
+			<v-text-field label="Regular" single-line :value="title"></v-text-field>
+		</v-col>
+		<v-textarea name="input-7-1" filled label="Content" auto-grow :value="content">
+			{{content}}
+		</v-textarea>
 	</div>
 </template>
 
 <script>
-export default {
-	name: 'Post',
-	props: {
-		title: {type: String},
-		content: {type: String}
+	export default {
+		name: 'Post',
+		props: {
+			title: {
+				type: String
+			},
+			content: {
+				type: String
+			}
+		}
 	}
-}
 </script>
 
 <style>
