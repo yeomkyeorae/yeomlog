0bd987aff35948d1a1becae673d0bf5f830c9e36
yeomkyeorae <duarufp06@naver.com>
2020-02-24 14:16:34 +0900

    200224 | connect post

--- frontend/src/components/PostList.vue
+++ frontend/src/components/PostList.vue
@@ -4,35 +4,16 @@
     class="mx-auto"
   >
     <v-container>
-      <v-row dense>
+      <v-row v-for="post in posts" :key="post.id" dense>
         <v-col cols="12">
           <v-card
             color="#385F73"
             dark
           >
-            <v-card-title class="headline">{{ categoryId }}Unlimited music now</v-card-title>
-            <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+            <v-card-title class="headline">{{ post.title }}</v-card-title>
+            <v-card-subtitle>{{ post.content }}</v-card-subtitle>
           </v-card>
         </v-col>
-        <v-col cols="12">
-          <v-card
-            color="#385F73"
-            dark
-          >
-            <v-card-title class="headline">Unlimited music now</v-card-title>
-            <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
-          </v-card>
-        </v-col>
-        <v-col cols="12">
-          <v-card
-            color="#385F73"
-            dark
-          >
-            <v-card-title class="headline">Unlimited music now</v-card-title>
-            <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
-          </v-card>
-        </v-col>
-
       </v-row>
     </v-container>
   </v-card>
@@ -58,10 +39,16 @@ export default {
       getInterests() {
         axios.get(`http://127.0.0.1:5000/interest/${this.categoryId}`)
           .then(response => {
+            this.posts = response.data
             console.log(response, 4444)
           })
       }
-    }
+    },
+    watch: { 
+      categoryId: function () {
+         this.getInterests() 
+      } 
+    },
 }
 </script>
 
