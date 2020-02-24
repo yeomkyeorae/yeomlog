64bea037c0f523154e96894861bbf0c9c171fa77
yeomkyeorae <duarufp06@naver.com>
2020-02-24 20:57:55 +0900

    200224 | add projects pictures

--- frontend/src/components/Project.vue
+++ frontend/src/components/Project.vue
@@ -7,11 +7,12 @@
 		</div>
 		<div>
 			<v-content>{{ intro }} {{ projectId }}</v-content>
+			<v-img :src="tmp"></v-img>
 		</div>
 		<br>
-		<div style="width:1000px; margin: 0 auto;">
-			<v-carousel :height="600" hide-delimiters>
-				<v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src" reverse-transition="fade-transition"
+		<div style="width:1200px; margin: 0 auto;">
+			<v-carousel :height="1000" hide-delimiters>
+				<v-carousel-item v-for="(item,i) in items[projectId]" :key="i" :src="item.src" reverse-transition="fade-transition"
 					transition="fade-transition"></v-carousel-item>
 			</v-carousel>
 		</div>
@@ -19,32 +20,117 @@
 </template>
 
 <script>
-// import axios from 'axios'
+	// import axios from 'axios'
 
 	export default {
 		name: 'project',
-		components : {
+		components: {
 			// axios
 		},
 		props: {
-			projectId: {type: Number}
+			projectId: {
+				type: Number
+			}
 		},
 		data() {
 			return {
 				intro: '해당 프로젝트는...',
-				items: [{
-						src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-					},
-					{
-						src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-					},
-					{
-						src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-					},
-					{
-						src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-					},
-				],
+				items: {
+					0: [
+						{ src: require('@/assets/Projects/Project1/1-01.jpg')},
+						{ src: require('@/assets/Projects/Project1/1-02.jpg')},
+						{ src: require('@/assets/Projects/Project1/1-03.jpg')},
+						{ src: require('@/assets/Projects/Project1/1-04.jpg')},
+						{ src: require('@/assets/Projects/Project1/1-05.jpg')},
+						{ src: require('@/assets/Projects/Project1/1-06.jpg')},
+						{ src: require('@/assets/Projects/Project1/1-07.jpg')},
+						{ src: require('@/assets/Projects/Project1/1-08.jpg')},
+						{ src: require('@/assets/Projects/Project1/1-09.jpg')},
+						{ src: require('@/assets/Projects/Project1/1-10.jpg')},
+						{ src: require('@/assets/Projects/Project1/1-11.jpg')},
+						{ src: require('@/assets/Projects/Project1/1-12.jpg')},
+						{ src: require('@/assets/Projects/Project1/1-13.jpg')},
+						{ src: require('@/assets/Projects/Project1/1-14.jpg')},
+						{ src: require('@/assets/Projects/Project1/1-15.jpg')},
+						{ src: require('@/assets/Projects/Project1/1-16.jpg')},
+						{ src: require('@/assets/Projects/Project1/1-17.jpg')},
+						{ src: require('@/assets/Projects/Project1/1-18.jpg')},
+						{ src: require('@/assets/Projects/Project1/1-19.jpg')},
+						{ src: require('@/assets/Projects/Project1/1-20.jpg')},
+					],
+					1: [
+						{ src: require('@/assets/Projects/Project2/2-01.jpg')},
+						{ src: require('@/assets/Projects/Project2/2-02.jpg')},
+						{ src: require('@/assets/Projects/Project2/2-03.jpg')},
+						{ src: require('@/assets/Projects/Project2/2-04.jpg')},
+						{ src: require('@/assets/Projects/Project2/2-05.jpg')},
+						{ src: require('@/assets/Projects/Project2/2-06.jpg')},
+						{ src: require('@/assets/Projects/Project2/2-07.jpg')},
+						{ src: require('@/assets/Projects/Project2/2-08.jpg')},
+						{ src: require('@/assets/Projects/Project2/2-09.jpg')},
+						{ src: require('@/assets/Projects/Project2/2-10.jpg')},
+						{ src: require('@/assets/Projects/Project2/2-11.jpg')},
+						{ src: require('@/assets/Projects/Project2/2-12.jpg')},
+						{ src: require('@/assets/Projects/Project2/2-13.jpg')},
+						{ src: require('@/assets/Projects/Project2/2-14.jpg')},
+						{ src: require('@/assets/Projects/Project2/2-15.jpg')},
+					],
+					2: [
+						{ src: require('@/assets/Projects/Project3/3-01.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-02.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-03.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-04.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-05.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-06.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-07.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-08.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-09.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-10.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-11.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-12.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-13.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-14.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-15.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-16.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-17.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-18.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-19.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-20.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-21.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-22.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-23.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-24.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-25.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-26.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-27.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-28.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-29.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-30.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-31.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-32.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-33.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-34.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-35.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-36.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-37.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-38.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-39.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-40.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-41.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-42.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-43.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-44.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-45.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-46.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-47.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-48.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-49.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-50.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-51.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-52.jpg')},
+						{ src: require('@/assets/Projects/Project3/3-53.jpg')},
+					]
+				}
 			}
 		},
 	}
