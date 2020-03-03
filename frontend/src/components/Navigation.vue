91f1bcd2af1f5d22f49c977e2e51016bc7d12e4b
yeomkyeorae <duarufp06@naver.com>
2020-03-03 22:32:52 +0900

    200303 | resolve errors

--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -2,22 +2,22 @@
   <div>
         <v-navigation-drawer v-model="sidebar">
       <v-list>
-        <v-list-tile
+        <v-list-item
           v-for="item in menuItems"
           :key="item.title"
           :to="item.path">
-          <v-list-tile-action>
+          <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
-        </v-list-tile>
+          </v-list-item-action>
+          <v-list-item-content>{{ item.title }}</v-list-item-content>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
     <v-toolbar>
       <span class="hidden-sm-and-up">
-        <v-toolbar-side-icon @click="sidebar = !sidebar">
-        </v-toolbar-side-icon>
+        <v-app-bar-nav-icon @click="sidebar = !sidebar">
+        </v-app-bar-nav-icon>
       </span>
       <v-toolbar-title>
         <router-link to="/" tag="span" style="cursor: pointer">
